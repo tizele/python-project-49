4d99192c8e4bb5908db48a944135f50a611b59d9
Timofei Zelenkin <thetizele@gmail.com>
2025-05-19 11:58:34 +0300

    fix game logic

--- brain_games/game.py
+++ brain_games/game.py
@@ -9,17 +9,16 @@ def game(names):
     name = welcome_user()
     print(f'Hello, {name}!')
     print(rules)
-    i = 0
     NUMBER_CORRECT_ANSWER = 3
-    while i < NUMBER_CORRECT_ANSWER:
+    for i in range(NUMBER_CORRECT_ANSWER):
         corect, quest, _ = names()
         print(f'Question: {quest}')
         answer = prompt.string('Your answer: ')
         if answer == str(corect):
             print('Correct!')
-            i += 1
+            if i == NUMBER_CORRECT_ANSWER - 1:
+                print(f'Congratulation, {name}!')
         else:
             print(f"'{answer}' is wrong answer ;(. Corect answer was '{corect}'.")
             print(f"Let's try again, {name}")
-            i = 0
-    print(f'Congratulation, {name}!')
+            break
